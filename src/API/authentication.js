3bbf05278154cae63e1f83f763ef995c22d16a92
binh-nguyen49-jh <binh.nguyen@journeyh.io>
2022-04-22 09:53:38 +0700

    test authentication module & API classes

--- src/API/authentication.js
+++ src/API/authentication.js
@@ -1,18 +1,12 @@
 import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword
-} from "@firebase/auth";
-import {
-  addDoc,
-  collection,
-  getDocs,
-  query,
-  where
-} from "@firebase/firestore/dist/lite";
+} from "firebase/auth";
+
 import {
   firebaseAuth,
-  firestoreDB
-} from "./firebaseApp";
+} from "./firebase";
+import UserAPI from "./userAPI";
 
 export const AUTHENTICATION_ERRORS = {
     NotExistProfile: "User profile does not exist"
@@ -23,30 +17,27 @@ export const logInWithEmail = (email, password) => {
     try {
       const res = await signInWithEmailAndPassword(firebaseAuth, email, password);
       const user = res.user;
-      const q = query(collection(firestoreDB, "users"), where("uid", "==", user.uid));
-      const docs = await getDocs(q);
-      if (docs.docs.length === 0) {
-        reject(AUTHENTICATION_ERRORS.NotExistProfile);
-      } 
-      resolve(docs.docs[0].data()); 
+      const userProfile = UserAPI.getUser(
+        user.uid
+      )
+      resolve(userProfile); 
     } catch (err) {
       reject(err)
     }
   })
 };
 
-export const registerWithEmail = (name, email, password, position) => {
+export const registerWithEmail = ({name, email, password, position}) => {
   return new Promise(async (resolve, reject) => {
     try {
       const res = await createUserWithEmailAndPassword(firebaseAuth, email, password);
       const user = res.user;
-      await addDoc(collection(firestoreDB, "users"), {
-        uid: user.uid,
+      await UserAPI.createUser({
+        uid: res.user.uid,
         name,
-        authProvider: "local",
         email,
         position
-      });
+      })
       resolve({
         uid: user.uid,
         name,
--- src/API/firebase.js
+++ src/API/firebase.js
@@ -3,7 +3,7 @@ import {initializeApp} from "firebase/app";
 import {getAuth} from "firebase/auth";
 import firebaseConfig from "../config/firebase";
 const firebaseApp = initializeApp(firebaseConfig);
-const firebaseAuth = getAuth(firebaseApp);
+const firebaseAuth =  getAuth();
 const firestoreDB = getFirestore(firebaseApp);
 
 export {firebaseApp, firebaseAuth, firestoreDB};
