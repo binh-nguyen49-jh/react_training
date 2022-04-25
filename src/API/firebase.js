a3a9a1652c1302ea9bbc117a5ebd4c6bb9d9c098
binh-nguyen49-jh <binh.nguyen@journeyh.io>
2022-04-25 11:01:36 +0700

    add authetication hooks

--- src/API/firebase.js
+++ src/API/firebase.js
@@ -1,9 +1,14 @@
 import { getFirestore } from "firebase/firestore/lite";
 import {initializeApp} from "firebase/app";
 import {getAuth} from "firebase/auth";
+import { getDatabase } from "firebase/database";
 import firebaseConfig from "../config/firebase";
 const firebaseApp = initializeApp(firebaseConfig);
 const firebaseAuth =  getAuth();
 const firestoreDB = getFirestore(firebaseApp);
-
+// Set up for detect ID token revocation
+const metadataRef = getDatabase().ref('metadata/' + uid);
+metadataRef.set({ revokeTime: utcRevocationTimeSecs }).then(() => {
+  console.log('Database updated successfully.');
+});
 export {firebaseApp, firebaseAuth, firestoreDB};
---
+++ src/hooks/authentication.js
@@ -0,0 +1,112 @@
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword
+} from "firebase/auth";
+
+import {
+  firebaseAuth,
+} from "./firebase";
+import {
+  createContext
+} from "react";
+
+const authContext = createContext();
+
+export function AuthProvider({
+  children
+}) {
+  const auth = useFirebaseAuth();
+  return (
+    <authContext.Provider value={auth}>
+      {children}
+    </authContext.Provider>
+  );
+}
+
+// For children to use authContext
+export const useAuth = () => {
+  return useContext(authContext);
+};
+
+function useFirebaseAuth() {
+  const [user, setUser] = useState(null);
+  const [idToken, setIdToken] = useState(null);
+
+  useEffect(() => {
+    const updateUser = firebaseAuth.onAuthStateChanged(async user => {
+      if (user) {
+        setUser(user);
+        
+        user.getIdToken(false).then(idToken => {
+          setIdToken(idToken);
+        }).catch(error => {
+          setIdToken(null);
+        });
+      } else {
+        setUser(null);
+        setIdToken(null);
+      }
+    });
+    return () => updateUser();
+  }, []);
+
+  const logInWithEmail = (email, password) => {
+      try {
+        const res = await signInWithEmailAndPassword(firebaseAuth, email, password);
+        const user = res.user;
+        setUser(user);
+      } catch (err) {
+        setUser(null);
+      }
+  };
+
+  const registerWithEmail = ({
+    name,
+    email,
+    password,
+    position
+  }) => {
+      try {
+        const res = await createUserWithEmailAndPassword(firebaseAuth, email, password);
+        const user = res.user;
+        await UserAPI.createUser({
+          uid: res.user.uid,
+          name,
+          email,
+          position
+        })
+        setUser(user);
+      } catch (err) {
+        setUser(null);
+      }
+  };
+
+  const logOut = () => {
+    firebaseAuth.signOut();
+    setUser(null);
+  };
+
+  const checkExpiration = async () => {
+    // check expiration by uid
+    if (!idToken) {
+        return false;
+    }
+    let checkRevocation = false;
+    firebaseAuth.verifyIdToken(idToken, checkRevocation).then((payload) => {
+      return true;
+    })
+    .catch((error) => {
+      logOut();
+      return false;
+    });;
+
+ };
+
+  return {
+    user,
+    idToken,
+    logInWithEmail,
+    registerWithEmail,
+    checkExpiration
+  };
+}
