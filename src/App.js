dd3836ef8003ba21098b305546b4ff1b4bc74a12
binh-nguyen49-jh <binh.nguyen@journeyh.io>
2022-04-25 16:07:19 +0700

    add profile page

--- src/App.js
+++ src/App.js
@@ -3,16 +3,23 @@ import {Routes, Route} from 'react-router-dom';
 import AuthenticationPage from './components/AuthenticationPage/AuthenticationPage';
 import HomePage from './components/HomePage/HomePage';
 import ProtectedRoute from './components/HOC/ProtectedRoute';
+import ProfilePage from './components/ProfilePage/ProfilePage';
 
 function App() {
   return (
     <Routes>
-      <Route element = { <AuthenticationPage />} path='/authentication' />
       <Route element = { 
         <ProtectedRoute>
           <HomePage />
         </ProtectedRoute>
-      } path='/' />
+      } path='/' exact/>
+      <Route element = { <AuthenticationPage />} path='/authentication' />
+      
+      <Route element = { 
+        <ProtectedRoute>
+          <ProfilePage />
+        </ProtectedRoute>
+      } path='/profile' />
       
     </Routes>
     
