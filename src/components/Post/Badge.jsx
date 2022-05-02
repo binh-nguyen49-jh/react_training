60c6f00d22d51fbfe5b993aacfcce24c1a1e89b1
binh-nguyen49-jh <binh.nguyen@journeyh.io>
2022-05-02 10:00:56 +0700

    define variables for css values

--- src/components/Post/Badge.jsx
+++ src/components/Post/Badge.jsx
@@ -6,14 +6,15 @@ function Badge({ text, style }) {
     <div
       className='badge'
       style={{
-        display: 'inline-block',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
         padding: '0.25em 0.4em',
         fontSize: '75%',
         fontWeight: '700',
         lineHeight: '1',
         textAlign: 'center',
         whitespace: 'nowrap',
-        verticalAlign: 'baseline',
         borderRadius: '0.25rem',
         backgroundColor: '#f8a81f',
         color: '#fff',
