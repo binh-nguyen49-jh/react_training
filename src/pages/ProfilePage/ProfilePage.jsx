28bd00771902bb59d991d178e2590256e5c93805
binh-nguyen49-jh <binh.nguyen@journeyh.io>
2022-05-11 15:25:51 +0700

    Get user profile by uid param in url

--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -1,5 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import UserAPI from '../../API/userAPI';
+import './ProfilePage.scss';
+
 export default function ProfilePage(props) {
+  // get uid from params
+  const { uid } = useParams();
+  const [userProfile, setUserProfile] = useState(null);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const userProfile = await UserAPI.getUserData(uid);
+      setUserProfile(userProfile);
+    };
+    fetchUser();
+  }, []);
+
   return (
     <main>
       <div className='container'>
