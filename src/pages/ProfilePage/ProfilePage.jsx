203d116cbc153ba3ca3baa2752dc8b3b9fd9d5a9
binh-nguyen49-jh <binh.nguyen@journeyh.io>
2022-05-11 15:59:55 +0700

    add avatar & status into profile

--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import UserAPI from '../../API/userAPI';
+import Avatar from '../../components/Avatar/Avatar';
 import './ProfilePage.scss';
 
 export default function ProfilePage(props) {
@@ -17,10 +18,36 @@ export default function ProfilePage(props) {
   }, []);
 
   return (
-    <main>
+    <main className='profilePage'>
       <div className='container'>
         <h1>Profile Page</h1>
+        {userProfile && <UserInfoSection {...userProfile} />}
       </div>
     </main>
+  );
+}
+
+function UserInfoSection({ avatar, bio, status, dob, position, name }) {
+  return (
+    <div className='userInfoSection'>
+      <div className='profileAvatar'>
+        <Avatar imgUrl={avatar} />
+        <div className='status'>
+          <div className='statusCircle'>
+            <div
+              className='circleInner'
+              style={{
+                backgroundColor: status
+                  ? 'var(--primary-cl)'
+                  : 'var(--gray-1-cl)',
+              }}
+            />
+          </div>
+        </div>
+      </div>
+      <div className='profileDescription'></div>
+      <div className='profilePosition'></div>
+      <div className='profileBio'></div>
+    </div>
   );
 }
